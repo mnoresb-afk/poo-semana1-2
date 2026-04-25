d9d3436f635055e808cd2fe86c7e6dbfbb9027ca
Maximo Nores Buendia <mnoresb@gmail.com>
2026-04-25 11:26:16 -0500

    feat: Agrego clase HolaMundo como prueba de la primera clase

---
+++ HolaMundo.java
@@ -0,0 +1,10 @@
+public class HolaMundo {
+    // Main es el punto de entrada de la aplicación
+    public static void main(String[] args){
+        // For
+        // Escribir un for que cuente de 0 a 4
+        for(int i = 0; i < 5; i++){
+            System.out.println(i);
+        }
+    }
+}
